72050fa5b3365e3af4d89f62cda5514b1e9a4a57
Andrew Lin <andrew.lin2@mail.mcgill.ca>
2018-11-11 00:02:30 -0500

    Comments and readme

--- Calculator.cpp
+++ Calculator.cpp
@@ -144,7 +144,7 @@ void printOperations() {
 double executeOperation(double operation, double firstNumber, double secondNumber) {
     double result;
     if (operation == (int)operation) {
-        int op = operation;
+        int op = operation; // Convert double to int for switch statement
         switch(op) {
             case 1:
                 result = addition(firstNumber, secondNumber);
@@ -320,6 +320,7 @@ int main() {
             startOver = 1;
         }
         else {
+            cout << "Good bye\n";
             return 0;
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Bad Calculator as practice with C++.
+
+This was worked on using Visual Studio Code with C/C++ extension. Standard is C++11. Can be changed in tasks.json.
+
+Json files are to configure the extension.
+
+For c_cpp_properties.json, modify "compilerPath" to the path of your gcc, depending on your OS.
