8578070115c14c09dd9cb76c55a05858ee17fb4b
andrew.lin2@mail.mcgill.ca <andrew.lin2@mail.mcgill.ca>
2018-11-30 23:05:02 -0500

    Fixes

--- Tests-Subtraction.cpp
+++ Tests-Subtraction.cpp
@@ -2,6 +2,6 @@
 #include "Calculator.h"
 
 TEST_CASE("Subtractions are computed", "[sub]") {
-    REQUIRE( subtraction(1.0, 1.0) == 0.0);
-    REQUIRE( subtraction(-1.0, -1.0) == 0.0);
+    REQUIRE(subtraction(1.0, 1.0) == 0.0);
+    REQUIRE(subtraction(-1.0, -1.0) == 0.0);
 }
--- Tests-addition.cpp
+++ Tests-addition.cpp
@@ -3,8 +3,8 @@
 
 // Something is wrong with computing more than 1 decimals. have to do with precision.
 TEST_CASE("Additions are computed", "[add]") {
-    REQUIRE( addition(1.0, 1.0) == 2.0 );
-    REQUIRE( addition(-1.0, -1.0) == -2.0 );
-    REQUIRE( addition(123.4, 678.9) == 802.3 );
-    REQUIRE( addition(-908.7, -543.2) == -1451.9 );
+    REQUIRE(addition(1.0, 1.0) == 2.0);
+    REQUIRE(addition(-1.0, -1.0) == -2.0);
+    REQUIRE(addition(123.4, 678.9) == 802.3);
+    REQUIRE(addition(-908.7, -543.2) == -1451.9);
 }
